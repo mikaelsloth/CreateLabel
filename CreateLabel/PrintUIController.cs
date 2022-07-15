d764011faed82e759857162eb2ff651de72bd31c
Mikael Sloth <mikael.sloth@tnt.dk>
2022-07-15 12:42:45 +0200

    Initial commit to allow others to work

---
+++ CreateLabel/PrintUIController.cs
@@ -0,0 +1,31 @@
+﻿namespace CreateLabel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal abstract class PrintUIController : UiController
+    {
+        public virtual string LabelForStylesheet => $"Stylesheet for {ApiName} Rendering";
+
+        public virtual string LabelForResponseSchema => $"Validate {ApiName} Response Schema";
+
+        public virtual string Stylesheet { get; set; } = "";
+
+        public virtual string StylesheetPath { get; set; } = "";
+
+        public virtual bool IsFOStylesheet { get; set; }
+
+        public virtual string ResponseSchema { get; set; } = "";
+
+        public virtual string ResponseSchemaPath { get; set; } = "";
+
+        public virtual bool ValidateResponseSchema { get; set; }
+
+        public virtual bool ShowDocument { get; set; }
+
+        public abstract void DisplayDocument();
+    }
+}
---
+++ CreateLabel/UiController.cs
@@ -0,0 +1,35 @@
+﻿namespace CreateLabel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal abstract class UiController
+    {
+        protected string ApiName = "-";
+
+        public virtual InputModeEnum InputMode { get; } = InputModeEnum.Request;
+
+        public virtual string LabelForRequestSchema => $"Validate {ApiName} Request Schema";
+
+        public virtual string LabelForInput => $"Paste your {InputMode} below";
+
+        public virtual string RequestSchema { get; set; } = "";
+
+        public virtual string RequestSchemaPath { get; set; } = "";
+
+        public virtual bool ValidateRequestSchema { get; set; }
+
+        public virtual string SaveOutputPath { get; set; } = "";
+
+        public virtual bool ShowReponse { get; set; }
+
+        public abstract void InitializeFormValues();
+
+        public abstract void RunCommand();
+
+        public abstract void DisplayResponse();
+    }
+}
